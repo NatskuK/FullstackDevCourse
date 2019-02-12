7f5e847e540f86ba9b6b437d65ca43647759deb5
Natalia Kaijalainen <natalia.kaijalainen@gmail.com>
2019-02-12 19:47:50 +0200

    Calculator exercise with option menu to select function

--- Calculator_section7/javascript/calc.js
+++ Calculator_section7/javascript/calc.js
@@ -1,18 +1,29 @@
-var numField1 = document.getElementById('numField1');
-var numField2 = document.getElementById('numField2');
+/*
+Variating exercise, instead of % calculating 
+1. Sum of X and Y
+2. Product of X and Y
+*/
+
+var numFieldX = document.getElementById('numFieldX');
+var numFieldY = document.getElementById('numFieldY');
 var resultField = document.getElementById('resultField');
 var form = document.getElementById('CalculateForm');
+var Selected = document.getElementById('Selected');
 
 form.addEventListener('submit', function(event) {
-    if (!numField1.value || !numField2.value){
+    if (!numFieldX.value || !numFieldY.value){
         alert("Please enter the numbers");
     } else{
-        var x = parseFloat(numField1.value); // parse string to be float number
-        var y = parseFloat(numField2.value);  
+        var x = parseFloat(numFieldX.value); // parse string to be float number
+        var y = parseFloat(numFieldY.value);  
+        var result = 0;
         
-        var result = x / y * 100;
-        resultField.innerHTML = "Result is " + result + "%";
+        if(parseInt(Selected.value) === 1)
+            result = x + y ; //sum if option 1 selected
+        else if(parseInt(Selected.value) === 2)
+            result = x * y; //product if option 2 selected
+        
+        resultField.innerHTML = "Result is " + result;
         event.preventDefault(); //stop refreshing page
-    }
-    
+    }  
 })
